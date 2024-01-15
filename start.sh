f76e42ea25fbd71486de54451f9fb2746b2c56ef
Lazyomkar <156710936+Lazyomkar@users.noreply.github.com>
2024-01-16 02:23:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
-cd boted;
+git clone https://github.com/Lazyomkar/Advance-Auto-Filter-n/Advance-Auto-Filter-n
+
+cd /Advance-Auto-Filter-n
 pip3 install -U -r requirements.txt;
 echo "Bot Started....";
 python3 bot.py;
